c78bd207bfd5a0e86d49fb8cb8a1783f490452c7
mathewdunne <73080543+mathewdunne@users.noreply.github.com>
2026-03-01 23:03:01 -0500

    added talonfx turret io

---
+++ src/main/java/frc/robot/subsystems/turret/TurretIOTalonFX.java
@@ -0,0 +1,87 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.turret;
+
+import static edu.wpi.first.units.Units.Hertz;
+import static edu.wpi.first.units.Units.Radians;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.configs.VoltageConfigs;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Voltage;
+import frc.robot.util.PhoenixUtil;
+
+public class TurretIOTalonFX implements TurretIO {
+  private final TalonFX motor;
+  private final Debouncer motorConnectedDebounce = new Debouncer(0.5);
+
+  private final TalonFXConfiguration turretConfig;
+
+  private final StatusSignal<Angle> position;
+  private final StatusSignal<AngularVelocity> velocity;
+  private final StatusSignal<Voltage> appliedVolts;
+  private final StatusSignal<Current> current;
+
+  private final double timeoutSeconds = 0.25;
+
+  public TurretIOTalonFX() {
+    motor = new TalonFX(TurretConstants.canId);
+
+    turretConfig =
+        new TalonFXConfiguration()
+            .withMotorOutput(
+                new MotorOutputConfigs()
+                    .withInverted(
+                        TurretConstants.motorInverted
+                            ? InvertedValue.Clockwise_Positive
+                            : InvertedValue.CounterClockwise_Positive)
+                    .withNeutralMode(NeutralModeValue.Brake))
+            .withCurrentLimits(
+                new CurrentLimitsConfigs()
+                    .withSupplyCurrentLimit(TurretConstants.currentLimit)
+                    .withSupplyCurrentLimitEnable(true))
+            .withVoltage(
+                new VoltageConfigs().withPeakForwardVoltage(12.0).withPeakReverseVoltage(-12.0))
+            .withFeedback(
+                new FeedbackConfigs().withSensorToMechanismRatio(1.0 / TurretConstants.gearRatio));
+
+    PhoenixUtil.tryUntilOk(5, () -> motor.getConfigurator().apply(turretConfig, timeoutSeconds));
+
+    position = motor.getPosition();
+    velocity = motor.getVelocity();
+    appliedVolts = motor.getMotorVoltage();
+    current = motor.getTorqueCurrent();
+
+    PhoenixUtil.registerStatusSignals(Hertz.of(50), position, velocity, appliedVolts, current);
+    motor.optimizeBusUtilization();
+  }
+
+  @Override
+  public void updateInputs(TurretIOInputs inputs) {
+    boolean motorStatus = BaseStatusSignal.isAllGood(position, velocity, appliedVolts, current);
+    inputs.motorConnected = motorConnectedDebounce.calculate(motorStatus);
+    inputs.positionRadians = position.getValue().in(Radians);
+    inputs.velocityRadiansPerSecond = velocity.getValue().in(RadiansPerSecond);
+    inputs.appliedVolts = appliedVolts.getValueAsDouble();
+    inputs.currentAmps = current.getValueAsDouble();
+  }
+
+  @Override
+  public void setMotorVoltage(double volts) {
+    motor.setVoltage(volts);
+  }
+}
---
+++ src/main/java/frc/robot/util/PhoenixUtil.java
@@ -0,0 +1,52 @@
+// Copyright (c) 2021-2026 Littleton Robotics
+// http://github.com/Mechanical-Advantage
+//
+// Use of this source code is governed by a BSD
+// license that can be found in the LICENSE file
+// at the root directory of this project.
+
+package frc.robot.util;
+
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.StatusSignalCollection;
+import edu.wpi.first.units.measure.Frequency;
+import java.util.function.Supplier;
+
+public class PhoenixUtil {
+  private static StatusSignalCollection signals = new StatusSignalCollection();
+
+  /** Attempts to run the command until no error is produced. */
+  public static void tryUntilOk(int maxAttempts, Supplier<StatusCode> command) {
+    for (int i = 0; i < maxAttempts; i++) {
+      var error = command.get();
+      if (error.isOK()) break;
+    }
+  }
+
+  /**
+   * Registers status signals with the specified frequency.
+   * @param frequency The frequency to set the status signals to.
+   * @param signals The status signals to register.
+   */
+  public static void registerStatusSignals(Frequency frequency, BaseStatusSignal... signals) {
+    BaseStatusSignal.setUpdateFrequencyForAll(frequency, signals);
+    PhoenixUtil.registerStatusSignals(signals);
+  }
+
+  /**
+   * Registers status signals with the specified frequency.
+   * @param signals The status signals to register.
+   */
+  public static void registerStatusSignals(BaseStatusSignal... signals) {
+    PhoenixUtil.signals.addSignals(signals);
+  }
+
+  /**
+   * Refreshes all registered status signals.
+   * @return The status code of the refresh operation.
+   */
+  public static StatusCode refreshAll() {
+    return PhoenixUtil.signals.refreshAll();
+  }
+}
