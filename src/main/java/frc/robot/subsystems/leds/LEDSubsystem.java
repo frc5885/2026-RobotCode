4054b56b92a827591dcdf2872becb6157a2e832f
mathewdunne <73080543+mathewdunne@users.noreply.github.com>
2026-03-22 10:20:30 -0400

    commandable LED zones

--- src/main/java/frc/robot/subsystems/leds/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/leds/LEDSubsystem.java
@@ -1,70 +1,76 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems.leds;
 
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLED.ColorOrder;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
-import edu.wpi.first.wpilibj.AddressableLEDBufferView;
 import edu.wpi.first.wpilibj.LEDPattern;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LEDSubsystem extends SubsystemBase {
   private static LEDSubsystem INSTANCE = null;
+
   private final AddressableLED leds;
   private final AddressableLEDBuffer buffer;
-  private final AddressableLEDBufferView hopperLong;
-  private final AddressableLEDBufferView turret;
-  private final AddressableLEDBufferView hopperShort;
-  private static LEDPattern ledState;
+
+  public final LEDZone hopperLong;
+  public final LEDZone turret;
+  public final LEDZone hopperShort;
 
   public static LEDSubsystem getInstance() {
-    if (INSTANCE == null) {
-      INSTANCE = new LEDSubsystem();
-    }
+    if (INSTANCE == null) INSTANCE = new LEDSubsystem();
     return INSTANCE;
   }
 
-  /** Creates a new LEDSubsystem. */
   private LEDSubsystem() {
     leds = new AddressableLED(LEDConstants.ledPort);
     buffer = new AddressableLEDBuffer(LEDConstants.length);
 
     leds.setLength(LEDConstants.length);
-    leds.setData(buffer);
     leds.setColorOrder(ColorOrder.kRGB);
-
-    ledState = LEDConstants.States.disabled;
     leds.start();
 
-    hopperLong = buffer.createView(0, LEDConstants.hopperLongLength - 1);
+    hopperLong =
+        new LEDZone(
+            "HopperLong",
+            buffer.createView(0, LEDConstants.hopperLongLength - 1),
+            LEDConstants.States.disabled);
+
     turret =
-        buffer.createView(
-            LEDConstants.hopperLongLength,
-            LEDConstants.hopperLongLength + LEDConstants.turretLength - 1);
+        new LEDZone(
+            "Turret",
+            buffer.createView(
+                LEDConstants.hopperLongLength,
+                LEDConstants.hopperLongLength + LEDConstants.turretLength - 1),
+            LEDConstants.States.disabled);
+
     hopperShort =
-        buffer.createView(
-            LEDConstants.hopperLongLength + LEDConstants.turretLength,
-            LEDConstants.hopperLongLength
-                + LEDConstants.turretLength
-                + LEDConstants.hopperShortLength
-                - 1);
+        new LEDZone(
+            "HopperShort",
+            buffer.createView(
+                LEDConstants.hopperLongLength + LEDConstants.turretLength,
+                LEDConstants.hopperLongLength
+                    + LEDConstants.turretLength
+                    + LEDConstants.hopperShortLength
+                    - 1),
+            LEDConstants.States.disabled);
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
-    ledState.applyTo(hopperLong);
-    LEDConstants.States.policeSirens.applyTo(turret);
-    ledState.applyTo(hopperShort);
+    // Zones write to buffer views in their own periodics;
+    // this flushes the final buffer state to hardware each tick.
     leds.setData(buffer);
   }
 
-  public static Command setLEDState(LEDPattern pattern) {
-    return new InstantCommand(() -> ledState = pattern, getInstance());
+  /**
+   * Applies a pattern across the entire physical strip as one continuous surface. Requires all
+   * three zones -- interrupts their individual commands while active. When this command ends, each
+   * zone reverts to its own default command.
+   */
+  public Command fullStripPattern(LEDPattern pattern) {
+    return Commands.run(() -> pattern.applyTo(buffer), hopperLong, turret, hopperShort)
+        .withName("FullStrip");
   }
 }
